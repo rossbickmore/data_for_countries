a7c01c8758dac0da783054d04901eca9f4264ea8
Ross Bickmore <rossbickmore@gmail.com>
2019-08-30 18:26:22 +0100

    created searchbox for countries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './Note'
 
-function App() {
+const App = () => {
+
+  const [ newCountry, setNewCountry] = useState("")
+  const [ countries, setCountries] = useState([]) 
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  const rows = () => countries.map(country =>
+    <Note 
+      name={country.name}
+      />
+  )
+
+
+
+  const handleCountryChange = (event) => {
+    event.preventDefault()
+    console.log(event.target.value)
+    setNewCountry(event.target.value)
+    const flags = 'gi'
+    const regex = new RegExp(`${newCountry}`, flags)
+    const filt = country => country.name.match(regex)
+    setCountries(countries.filter(filt))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data for Countries!</h1>
+      <div>
+        find countries <input defaultValue={newCountry} onChange={handleCountryChange} />
+      </div>
+      <h2>Countries found</h2>
+        <div>
+          <ul>
+            {rows()}
+          </ul>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
